10179ce358bffb26e762dd2e294d00487f8a6457
Dominik Šelmeci <dominik.selmeci@gmail.com>
2024-04-13 21:45:37 +0200

    fix: damaging totem by clicking totem

--- Assets/Scripts/Totem.cs
+++ Assets/Scripts/Totem.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Totem : MonoBehaviour
 {
     private float _health = 1f;
-
-    private void OnMouseDown()
-    {
-        _health -= 0.15f;
-        if (_health < 0) Destroy(gameObject);
-        Debug.Log("clicked Totem");
-        GetComponentInChildren<TotemHealth>().SetHealth(_health);
-    }
 
     public void DamageIncoming()
     {
